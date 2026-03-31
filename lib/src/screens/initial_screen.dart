0e159baa673f754231de964f6f0688a6679957dd
PedroIvo027 <pedroivotavaresp@gmail.com>
2026-03-31 11:09:39 -0300

    feat: adciona base do projeto flutter

---
+++ lib/src/screens/initial_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_usedev/src/widgets/hero_section_widget.dart';
+import 'package:projeto_usedev/src/widgets/product_card_widget.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class InitialScreen extends StatefulWidget{
+  const InitialScreen({super.key});
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+
+  final List<Map<String, String>> produtos = [
+    {
+      'name' : 'Camiseta Capy',
+      'price': '28,00',
+      'image': 'assets/camisa_capy.png'
+    },
+    {
+      'name' : 'Mousepad Café',
+      'price': '18,00',
+      'image': 'assets/mousepad_cafe.png' 
+    },
+    {
+      'name' : 'Caneca Bug',
+      'price': '28,00',
+      'image': 'assets/caneca_bug.png'
+    },
+    {
+      'name' : 'Boné 404',
+      'price': '25,00',
+      'image': 'assets/bone_404.png'
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon( Icons.menu, size:40),
+        title: Image.asset('assets/logo_usedev.png', height:40),
+        centerTitle: true,
+        actions: const [
+          Icon(Icons.person_outline, size: 40),
+          SizedBox(width: 10),
+          Icon(Icons.shopping_cart_outlined, size: 40),
+          SizedBox(width: 25),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children:  [
+            HeroSection(),
+            
+            Text(
+              'Promos Especiais',
+              style: TextStyle(
+                fontFamily: GoogleFonts.orbitron().fontFamily,
+                fontSize: 27,
+                fontWeight: .bold,
+                color: Colors.black87,
+              ),
+              ),
+              
+            
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: produtos.length,
+              itemBuilder: (context, index){
+                final produto = produtos[index];
+
+                return ProductCardWidget(
+                  name: produto['name']!,
+                  price: produto ['price']!,
+                  image: produto['image']!
+                );
+              },
+            ), 
+
+            ],
+          ),
+        ),
+      );
+  }
+}
